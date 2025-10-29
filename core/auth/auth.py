cfed00340acf46c4868477449ea454238cf25572
Joao Assalim <joassalim@gmail.com>
2025-10-28 21:59:44 -0300

    feat: function to handle api key in requests

---
+++ core/auth/auth.py
@@ -0,0 +1,18 @@
+import uuid
+from fastapi import Security, HTTPException, status, Request
+from fastapi.security import APIKeyHeader
+from sqlmodel import Session, select
+
+from database.config import engine
+from database.models.users import UserModel
+
+api_key = APIKeyHeader(name="x-api-key")
+
+async def validate_api_key(api_key: str = Security(api_key)) -> UserModel:
+    with Session(engine) as session:
+        api_key_db = session.exec(select(UserModel).where(UserModel.api_key == uuid.UUID(api_key))).first()
+
+        if not api_key_db:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid API key")
+
+        return api_key_db
