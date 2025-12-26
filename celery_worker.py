87b23c5d375ef1839263d582dcb0c2f8d59c2f09
Joao Assalim <joassalim@gmail.com>
2025-12-26 13:07:01 -0300

    fix: ctx to celery call

--- celery_worker.py
+++ celery_worker.py
@@ -6,9 +6,12 @@ from pydantic_ai import RunContext
 
 from core.api import AgentsAPIView
 from core.agents.base_agent import AgentDeps
+from core.database.mongo import DatabaseHandler
 from core.agents.agent_orchestrator import OrchestratorAgent
 
 load_dotenv()
+
+MONGO_HISTORY_COLLECTION = os.getenv("MONGODB_HISTORY_COLLECTION")
 
 celery = Celery(
     "worker",
@@ -21,8 +24,14 @@ def execute_agent_task(message, user_id, agent_id, ctx: RunContext[AgentDeps]):
     try:
         agent = AgentsAPIView().get_agent(agent_id, user_id)
 
+        deps = AgentDeps(
+            db=DatabaseHandler(MONGO_HISTORY_COLLECTION),
+            user_id=str(user_id),
+            agent_id=str(agent_id)
+        )
+
         agent = OrchestratorAgent(agent)
-        response = agent.execute(message, is_tool_agent=False, deps=ctx)
+        response = agent.execute(message, is_tool_agent=False, deps=deps)
         return {"message": response}
     except Exception as e:
         return {"error": str(e)}
